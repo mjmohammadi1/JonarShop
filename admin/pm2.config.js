7cdbda182b728923de6e955d137231937bb8a38e
Jafar <mjmohammadi1@gmail.com>
2021-11-17 18:44:08 -0500

    refactor: update pm2 commands

--- admin/pm2.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: "jonar-admin",
-      script: "npm run start",
-      watch: true,
-    },
-  ],
-};
--- client/pm2.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: "jonar-client",
-      script: "npm run start",
-      watch: true,
-    },
-  ],
-};
